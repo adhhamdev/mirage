ea3c66033e74801f35f223d3f2bc031aed08c768
Adhham Safwan <msmadhhamg@gmail.com>
2024-10-07 00:14:20 +0530

    add edge config

--- app/api/generate-image/route.js
+++ app/api/generate-image/route.js
@@ -7,19 +7,18 @@ export async function POST(req) {
   try {
     const { prompt, width, height } = await req.json();
 
+    console.log('Generating image:', prompt, width, height);
+
     const response = await fetch(
       'https://api-inference.huggingface.co/models/black-forest-labs/FLUX.1-dev',
       {
         headers: {
           Authorization: `Bearer ${process.env.HUGGINGFACE_API_KEY}`,
+          'x-wait-for-model': true,
         },
         method: 'POST',
         body: JSON.stringify({
           inputs: prompt,
-          parameters: {
-            num_inference_steps: 100,
-            target_size: { width, height },
-          },
         }),
       }
     );
@@ -33,6 +32,8 @@ export async function POST(req) {
     const { url } = await put('images/image.png', result, {
       access: 'public',
     });
+
+    console.log('Image generated:', url);
 
     return NextResponse.json({ imageUrl: url });
   } catch (error) {
