8dfb79bb07a472117d20b842f87c7c06793b5432
Adhham Safwan <msmadhhamg@gmail.com>
2024-10-09 16:55:20 +0530

    add icon

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,3 @@
-import { Poppins } from 'next/font/google';
-
-const poppins = Poppins({
-  subsets: ['latin'],
-  weight: ['400', '500', '600', '700'],
-});
-
 export const metadata = {
   title: 'Mirage AI - AI-Powered Image Generation',
   description:
@@ -45,7 +38,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
-      <body className={poppins.className}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
--- app/page.js
+++ app/page.js
@@ -1,15 +1,22 @@
 import ImageGenerationInterface from '@/components/ImageGenerationInterface';
+import { Poppins } from 'next/font/google';
 import './globals.css';
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'],
+});
 
 export default function Home() {
   return (
-    <main className='flex flex-col justify-center items-center p-4 min-h-screen bg-gradient-to-b from-white to-gray-100 sm:p-6 md:p-8'>
+    <main
+      className={`flex flex-col justify-center items-center p-4 min-h-screen bg-gradient-to-b from-white to-gray-100 sm:p-6 md:p-8 ${poppins.className}`}>
       <div className='w-full max-w-3xl'>
-        <h1 className='mb-8 text-4xl font-extrabold text-center sm:text-5xl md:text-6xl'>
+        <h1 className='mb-8 text-4xl font-bold text-center sm:text-5xl md:text-6xl'>
           <span className='bg-clip-text text-transparent bg-gradient-to-r from-gray-800 via-slate-700 to-zinc-600 drop-shadow-[0_2px_2px_rgba(255,255,255,0.1)]'>
             Mirage AI
           </span>
-          <span className='block mt-2 text-lg font-semibold text-gray-600 sm:text-xl md:text-2xl'>
+          <span className='block mt-2 text-lg text-gray-600 sm:text-xl md:text-2xl'>
             Imagine. Generate. Create.
           </span>
         </h1>
